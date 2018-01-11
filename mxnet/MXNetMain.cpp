c8c35caeb7fc6c4691becc5aae760168b6637f80
dengmingxuan@hotmail.com <dengmingxuan@hotmail.com>
2018-01-11 23:45:34 +0800

    automake setup ready

---
+++ mxnet/MXNetMain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "SgDebug.h"
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+  cout << "testing." << endl;
+
+  SgDebug() << "Debug tetsing. " << "\n";
+  return 0;
+}
---
+++ smartgame/SimpleMain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+  cout << "testing." << endl;
+
+  // SgDebug() << "Debug tetsing. " << "\n";
+  return 0;
+}
